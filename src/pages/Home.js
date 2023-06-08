4a330b299e42370f5d1b54c13e3d9a2ba9a04772
Cindy Wong <cindywongdev@gmail.com>
2023-06-08 19:06:42 -0400

    changed location from new york to brooklyn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = (props) => {
                 <div class="text-center">
                     <h1 class="name-title">Cindy Wong</h1>
                     <h2 class="job-title">Software Engineer</h2>
-                    <h3 class="location-title">New York, NY</h3>
+                    <h3 class="location-title">Brooklyn, NY</h3>
                     <Header></Header>
                     <a href="#work"><div class="down-button"><AiOutlineArrowDown/></div></a>
                 </div>
