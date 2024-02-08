84dbf113647e9d563087db983f33384a5575b4be
Cindy Wong <cindywongdev@gmail.com>
2024-02-08 15:32:59 -0500

    added pic to about section:

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,18 +12,24 @@ const About = (props) => {
         <a id="about"></a>
         <h1 class="section-heading text-center">About Cindy</h1>
         <div className="about" style={aboutStyling}>
-            <h2>{about.name}</h2>
-            <h3>{about.email}</h3>
-            <p>Brooklyn-born and raised software engineer with a passion for making a positive impact on the people and world around me. With a bachelor’s degree in psychology and experience in customer service, I aim to tackle problems with a user-centered approach, understanding the human behind the screen. I am a quick learner who is able to fuse my proficiency in graphic design software with newly learned technologies such as React and CSS, allowing me to develop effective and elegant technical solutions.</p>
-            <h3>Technical Skills</h3>
-            <p>Languages - JavaScript, HTML5, CSS3, Python | Libraries and Frameworks - Express.js, React.js, Node.js, jQuery, SASS, TailwindCSS, Bootstrap, Flask | Databases -  MongoDB, PostgreSQL  | Other - Agile, Scrum, RESTful Routing, JSON, MVC frameworks, Postman, Github, VSCode, Trello, Figma, Adobe Suite (Illustrator, Photoshop, InDesign, XD), Microsoft Office, Canva</p>
+            <div className="about-text">
+                <h2>{about.name}</h2>
+                <h3>{about.email}</h3>
+                <p>Brooklyn-born and raised software engineer with a background in psychology. Wanting to impact the world at a larger level, I shifted my focus from driving change at the individual level to the societal level. I am well versed in JavaScript and React, and I’m particularly passionate about creating impactful user-centric frontend experiences addressing broader societal issues, especially mental health and wellness.</p>
+                <h3>Technical Skills</h3>
+                <p>Programming Languages: JavaScript, HTML, CSS, Python, SQL</p>
+                <p>Libraries and Frameworks: React, Vue, NodeJS, Express.js, jQuery, SASS, TailwindCSS, Bootstrap, Django, Flask
+    Databases: MongoDB, PostgreSQL</p>
+    <p>Other: Agile, REST, MVC, Jest, Postman, Git, Github, Auth0, Jira, Figma, Adobe Suite (Illustrator, Photoshop, InDesign, XD)</p>
 
-            <h2>Contact Me</h2>
-            <p>{about.email}</p>
-            <a href="#">https://github.com/cindywongdev</a>
-            <br></br>
-            <a href="#">https://linkedin.com/cindywongdev</a>
-            
+                <h2>Contact Me</h2>
+                <p>{about.email}</p>
+                <a href="#">https://github.com/cindywongdev</a>
+                <br></br>
+                <a href="#">https://linkedin.com/cindywongdev</a>
+            </div>  
+
+            <img className="about-img" alt="Cindy in Coit Tower, smiling by two windows overlooking San Francisco's cityscape." src="https://i.imgur.com/pmTUZ8Q.jpg"/>
         </div>
         <a href="#top"><div class="up-button"><AiOutlineArrowUp/></div></a>
     </main></>)
